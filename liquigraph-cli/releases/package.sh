f7720e3538fd204060e37aad66201daff2df08bd
Florent Biville <florent.biville@gmail.com>
2014-11-30 22:02:15 +0100

    Issues #25,#26,#27. Various issues with CLI
     - rewrite script in Python (embedded JAR won't work on Windows)
     - do not surround classpath with single quote when execution
    client via cmd.exe
     - assume current directory when none extracted

--- liquigraph-cli/releases/package.sh
+++ liquigraph-cli/releases/package.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 FOLDER=$(git rev-parse --show-toplevel || echo ".");
 FOLDER="$FOLDER/liquigraph-cli"
-(cd $FOLDER; cat target/classes/liquigraph target/liquigraph.jar > target/liquigraph.run && chmod +x target/liquigraph.run)
+(cd $FOLDER; zip -j target/liquigraph.zip target/classes/liquigraph target/liquigraph.jar)
 
