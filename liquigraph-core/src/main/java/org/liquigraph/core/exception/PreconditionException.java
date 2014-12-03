f791bdd6c5bb66e47a618675c62d1faef0a29bd5
Florent Biville <florent.biville@gmail.com>
2014-12-03 10:40:11 +0100

    I see... dead code

--- liquigraph-core/src/main/java/org/liquigraph/core/exception/PreconditionException.java
+++ liquigraph-core/src/main/java/org/liquigraph/core/exception/PreconditionException.java
@@ -4,10 +4,6 @@ import static java.lang.String.format;
 
 public class PreconditionException extends RuntimeException {
 
-    public PreconditionException(Throwable cause, String message, Object... arguments) {
-        super(format(message, arguments), cause);
-    }
-
     public PreconditionException(String message, Object... arguments) {
         super(format(message, arguments));
     }
