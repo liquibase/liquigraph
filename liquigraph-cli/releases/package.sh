339b188cdbd5305071271b7de538e9a7ea0c9399
@fbiville <florent.biville@gmail.com>
2015-01-20 16:15:11 +0100

    Issue #33. Rewrite CLI and package as ZIP.
    Rely on URLClassLoader rather than scripting classpath definition.
    
     - two basic scripts (shell+batch) taken from appassembler plugin
    execution
     - one minified shaded JAR containing the client
    
    Default deploy to Maven Central is disabled in favor of ZIP
    deploy. Alas, the whole thing required some hacky configuration of
    the loathed maven-antrun-plugin and maven-deploy-plugin.

--- liquigraph-cli/releases/package.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-FOLDER=$(git rev-parse --show-toplevel || echo ".");
-FOLDER="$FOLDER/liquigraph-cli"
-(cd $FOLDER; zip -j target/liquigraph.zip target/classes/liquigraph target/liquigraph.jar)
-
