f0e6d44eafc8ad1da19ddd67e9184b0d6ff4214d
Florent Biville <florent.biville@gmail.com>
2015-05-16 15:50:18 +0200

    [#40] Separate tx for execution and history insert

--- liquigraph-core/src/main/java/org/liquigraph/core/writer/ChangelogGraphWriter.java
+++ liquigraph-core/src/main/java/org/liquigraph/core/writer/ChangelogGraphWriter.java
@@ -3,6 +3,7 @@ package org.liquigraph.core.writer;
 import org.liquigraph.core.exception.PreconditionNotMetException;
 import org.liquigraph.core.model.Changeset;
 import org.liquigraph.core.model.Precondition;
+import org.liquigraph.core.model.PreconditionErrorPolicy;
 
 import java.sql.*;
 import java.util.Collection;
@@ -11,6 +12,7 @@ import java.util.Map;
 
 import static com.google.common.base.Throwables.propagate;
 import static java.lang.String.format;
+import static org.liquigraph.core.model.PreconditionErrorPolicy.MARK_AS_EXECUTED;
 
 public class ChangelogGraphWriter implements ChangelogWriter {
 
@@ -37,43 +39,55 @@ public class ChangelogGraphWriter implements ChangelogWriter {
         this.preconditionExecutor = preconditionExecutor;
     }
 
+    /**
+     * Runs the set of migrations against the configured database and inserts them
+     * in the persisted migration graph.
+     *
+     * Please note that these two operations are performed in two separate transactions,
+     * as user-defined migrations may operate on indices and those need be run apart
+     * from data changes.
+     */
     @Override
     public void write(Collection<Changeset> changelogsToInsert) {
         long index = latestPersistedIndex(connection) + 1L;
 
         for (Changeset changeset : changelogsToInsert) {
-            Precondition precondition = changeset.getPrecondition();
-
-            try (Statement statement = connection.createStatement()) {
-                PreconditionResult preconditionResult = executePrecondition(precondition);
-                if (preconditionResult == null || preconditionResult.executedSuccessfully()) {
-                    statement.execute(changeset.getQuery());
-                    upsertChangeset(connection, index, changeset);
-                }
-                else {
-                    switch (preconditionResult.errorPolicy()) {
-                        case CONTINUE:
-                            continue;
-                        case MARK_AS_EXECUTED:
-                            upsertChangeset(connection, index, changeset);
-                            break;
-                        case FAIL:
-                            throw new PreconditionNotMetException(
-                                format(
-                                    "Changeset <%s>: precondition query %s failed with policy <%s>. Aborting.",
-                                    changeset.getId(),
-                                    precondition.getQuery(),
-                                    precondition.getPolicy()
-                                )
-                            );
-                    }
-                }
-                connection.commit();
-            } catch (SQLException e) {
-                throw propagate(e);
+            StatementExecution statementExecution = executeStatement(changeset);
+            if (statementExecution == StatementExecution.IGNORE_FAILURE) {
+                continue;
             }
-            index++;
+
+            insertChangeset(connection, index++, changeset);
         }
+    }
+
+    private StatementExecution executeStatement(Changeset changeset) {
+        try (Statement statement = connection.createStatement()) {
+            Precondition precondition = changeset.getPrecondition();
+            PreconditionResult preconditionResult = executePrecondition(precondition);
+            if (preconditionResult != null && !preconditionResult.executedSuccessfully()) {
+                switch (preconditionResult.errorPolicy()) {
+                    case CONTINUE:
+                        return StatementExecution.IGNORE_FAILURE;
+                    case FAIL:
+                        throw new PreconditionNotMetException(
+                            format(
+                                "Changeset <%s>: precondition query %s failed with policy <%s>. Aborting.",
+                                changeset.getId(),
+                                precondition.getQuery(),
+                                precondition.getPolicy()
+                            )
+                        );
+                }
+            } else if (preconditionResult == null || preconditionResult.executedSuccessfully()
+                                                  || preconditionResult.errorPolicy() == MARK_AS_EXECUTED) {
+                statement.execute(changeset.getQuery());
+            }
+            connection.commit();
+        } catch (SQLException e) {
+            throw propagate(e);
+        }
+        return StatementExecution.SUCCESS;
     }
 
     private PreconditionResult executePrecondition(Precondition precondition) {
@@ -86,13 +100,14 @@ public class ChangelogGraphWriter implements ChangelogWriter {
         return result;
     }
 
-    private void upsertChangeset(Connection connection, long index, Changeset changeset) {
+    private void insertChangeset(Connection connection, long index, Changeset changeset) {
         Map<Integer, Object> parameters = parameters(changeset, index);
         try (PreparedStatement statement = connection.prepareStatement(CHANGESET_UPSERT)) {
             for (Integer key : parameters.keySet()) {
                 statement.setObject(key, parameters.get(key));
             }
             statement.execute();
+            connection.commit();
         } catch (SQLException e) {
             throw propagate(e);
         }
@@ -102,7 +117,10 @@ public class ChangelogGraphWriter implements ChangelogWriter {
         try (Statement statement = connection.createStatement()) {
             ResultSet resultSet = statement.executeQuery(LATEST_INDEX);
             resultSet.next();
-            return resultSet.getLong("lastIndex");
+            long lastIndex = resultSet.getLong("lastIndex");
+
+            connection.commit();
+            return lastIndex;
         } catch (SQLException e) {
             throw propagate(e);
         }
@@ -118,5 +136,9 @@ public class ChangelogGraphWriter implements ChangelogWriter {
         parameters.put(4, query);
         parameters.put(5, changeset.getAuthor());
         return parameters;
+    }
+
+    private static enum StatementExecution {
+        SUCCESS, IGNORE_FAILURE;
     }
 }
