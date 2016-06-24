efb340dddc7550d60754f434888bc25d336ab4b2
Frank Pavageau <fpavageau@ekino.com>
2016-06-25 00:15:48 +0200

    Wait for the dockerized Neo4j to become available

--- build/run.sh
+++ build/run.sh
@@ -3,5 +3,10 @@ set -e
 if [ "$WITH_DOCKER" = true ] ; then
     docker pull neo4j:${NEO_VERSION}
     docker run --detach --publish=7474:7474 --volume=$HOME/neo4j/data:/data --env=NEO4J_AUTH=neo4j/j4oen neo4j
+    # Wait up to 30s for Neo4j to become available
+    for i in {1..30}; do
+        curl -s localhost:7474/db/data -o /dev/null && break
+        sleep 1
+    done
 fi
 mvn -T4 clean test -Dneo4j.version=${NEO_VERSION} ${EXTRA_PROFILES}
