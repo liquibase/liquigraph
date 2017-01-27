2c98189a9894f681e8a0dc99e9cbd7d2694dc780
Florent Biville <f.biville@criteo.com>
2017-01-27 09:07:06 +0100

    Cover legacy schema locations and fix URLs

--- liquigraph-core/src/test/java/org/liquigraph/core/validation/XmlSchemaValidatorTest.java
+++ liquigraph-core/src/test/java/org/liquigraph/core/validation/XmlSchemaValidatorTest.java
@@ -98,11 +98,10 @@ public class XmlSchemaValidatorTest {
 
     @Test
     public void parses_changelog_with_explicit_schema() throws Exception {
-        Collection<String> errors = validator.validateSchema(
-            asNode("changelog/changelog-with-schema-location.xml")
-        );
-
-        assertThat(errors).isEmpty();
+        assertThat(validator.validateSchema(asNode("changelog/changelog-with-old-RC3-schema-location.xml"))).isEmpty();
+        assertThat(validator.validateSchema(asNode("changelog/changelog-with-old-schema-location.xml"))).isEmpty();
+        assertThat(validator.validateSchema(asNode("changelog/changelog-with-RC3-schema-location.xml"))).isEmpty();
+        assertThat(validator.validateSchema(asNode("changelog/changelog-with-schema-location.xml"))).isEmpty();
     }
 
     private Node asNode(String path) throws Exception {
