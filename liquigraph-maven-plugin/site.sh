332e94a71906eab39232f70907ba4f5e948bdc4a
Florent Biville <florent.biville@gmail.com>
2018-08-03 12:14:02 +0200

    Improve Maven plugin version parsing

--- liquigraph-maven-plugin/site.sh
+++ liquigraph-maven-plugin/site.sh
@@ -1,9 +1,7 @@
 #!/bin/zsh
 set -ex
 
-CURRENT_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate \
-			-Dexpression=project.version 2> /dev/null \
-			| grep '^\d')
-VERSION=$(echo $CURRENT_VERSION | sed 's/-SNAPSHOT//')
+CURRENT_VERSION=$(mvn -N -q org.apache.maven.plugins:maven-help-plugin:3.1.0:evaluate -Dexpression=project.version -DforceStdout=true)
+VERSION=$(echo $CURRENT_VERSION | sed 's/-SNAPSHOT.*//')
 mvn -B -q site 2> /dev/null
 for file in `ls **/*.html`; do sed -i .bak "s/$CURRENT_VERSION/$VERSION/" $file; done
