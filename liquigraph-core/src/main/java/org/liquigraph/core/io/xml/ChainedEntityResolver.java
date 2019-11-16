b2b883365da30d630797df40427b1801984a2620
Matt <mail4mattharrison@gmail.com>
2019-11-16 22:31:15 +1300

    [#241] Allow offline usage of Liquigraph
    This introduces a chained entity resolver which searches first the
    local directory for known XSDs, and then delegate to the
    online, redirect-aware resolver if not found.
    
    The local resolver now just uses a lookup of fixed paths, to ensure
    there could be no path traversal problems by using a dynamic lookup.

---
+++ liquigraph-core/src/main/java/org/liquigraph/core/io/xml/ChainedEntityResolver.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2014-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.liquigraph.core.io.xml;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.xml.sax.EntityResolver;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+public class ChainedEntityResolver implements EntityResolver {
+
+    private final List<EntityResolver> resolvers = new ArrayList<>();
+
+    public void addEntityResolver(final EntityResolver resolver) {
+        resolvers.add(resolver);
+    }
+
+    @Override
+    public InputSource resolveEntity(String publicId, String systemId) throws SAXException, IOException {
+        for (EntityResolver entityResolver : resolvers) {
+            InputSource entity = entityResolver.resolveEntity(publicId, systemId);
+            if (entity != null) {
+                return entity;
+            }
+        }
+        return null;
+    }
+}
---
+++ liquigraph-core/src/test/java/org/liquigraph/core/io/xml/ImplicitSchemaValidatorOfflineTest.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2014-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.liquigraph.core.io.xml;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.liquigraph.core.io.xml.DomSources.domStringSource;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Collection;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.dom.DOMSource;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.w3c.dom.Node;
+
+import io.sniffy.socket.DisableSockets;
+import io.sniffy.test.junit.SniffyRule;
+
+public class ImplicitSchemaValidatorOfflineTest {
+
+    @Rule
+    public SniffyRule sniffyRule = new SniffyRule();
+
+    @Test
+    @DisableSockets
+    public void validates_document_with_no_internet_connection() throws Exception {
+        String document =
+                "<changelog>\n" +
+                "    <changeset id=\"hello-world\" author=\"you\">\n" +
+                "        <query>CREATE (n:Sentence {text:'Hello monde!'}) RETURN n</query>\n" +
+                "    </changeset>" +
+                "</changelog>";
+        ImplicitSchemaValidator validator = new ImplicitSchemaValidator();
+
+        Collection<String> errors = validator.validate(domStringSource(document));
+
+        assertThat(errors).isEmpty();
+    }
+}
